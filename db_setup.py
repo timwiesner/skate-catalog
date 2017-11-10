0f4829240be7749423b092494666d03ffac96375
timwiesner <timothyjwiesner@gmail.com>
2017-11-10 10:05:58 -0600

    fix: Remove unnecessary category name

--- db_setup.py
+++ db_setup.py
@@ -29,7 +29,6 @@ class Item(Base):
     name = Column(String(80), nullable = False)
     id = Column(Integer, primary_key = True)
     description = Column(String(250))
-    category_name = Column(String(80))
     category_id = Column(Integer, ForeignKey('category.id'))
     category = relationship(Category)
 
