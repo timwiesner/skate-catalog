66b4c16178ee6ffb1b910dd75dbcb5b05590d9b4
timwiesner <timothyjwiesner@gmail.com>
2017-11-10 10:46:59 -0600

    feat: Render catalog.html template

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, jsonify, url_for
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from db_setup import Base, Category, Item
@@ -11,10 +11,18 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind = engine)
 session = DBSession()
 
+# Home page route
 @app.route('/')
 @app.route('/catalog')
 def hello():
-    return "Hello world"
+    return render_template('catalog.html')
+
+# # Show category route
+# @app.route('/catalog/<str:category_id>')
+# def showCategory(category_id):
+#     category = session.query(Category).filter_by(name=category_id).one()
+#     items = session.query(Item).filter_by(category_id=category_id).all()
+
 
 
 if __name__ == '__main__':
