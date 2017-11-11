e250308dcd2f0922fa4d09c736b76ded8586f33b
timwiesner <timothyjwiesner@gmail.com>
2017-11-11 16:50:06 -0600

    feat: Add edit route

--- application.py
+++ application.py
@@ -20,7 +20,7 @@ def showCatalog():
 
 
 # Create a new category
-@app.route('/catalog/new', methods = ['GET', 'POST'])
+@app.route('/catalog/new/', methods = ['GET', 'POST'])
 def newCategory():
     if request.method == 'POST':
         newCategory = Category(name=request.form['name'])
@@ -31,9 +31,19 @@ def newCategory():
         return render_template('newCategory.html')
 
 
+# Edit category
+@app.route('/catalog/<int:category_id>/edit/', methods = ['GET', 'POST'])
+def editCategory(category_id):
+    editedCategory = session.query(Category).filter_by(id=category_id).one()
+    if request.method == 'POST':
+        if request.form['name']:
+            editedCategory.name = request.form['name']
+            return redirect(url_for('showCatalog'))
+    else:
+        return render_template('editCategory.html', category=editedCategory)
 
 # Show category route
-@app.route('/catalog/<int:category_id>')
+@app.route('/catalog/<int:category_id>/')
 def showCategory(category_id):
     category = session.query(Category).filter_by(id=category_id).one()
     # items = session.query(Item).filter_by(category_id=category_id).all()
