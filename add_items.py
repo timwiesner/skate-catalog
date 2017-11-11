1244cce4c2cdcc5bdd6b9eb034c31b344cc40e5f
timwiesner <timothyjwiesner@gmail.com>
2017-11-11 15:14:58 -0600

    feat: Add item categories

--- add_items.py
+++ add_items.py
@@ -11,4 +11,46 @@ session = DBSession()
 
 
 
-category1 = Category(name="")
+category1 = Category(name="Complete Skateboards")
+session.add(category1)
+session.commit()
+
+category2 = Category(name="Skateboard Decks")
+session.add(category1)
+session.commit()
+
+category3 = Category(name="Cruisers")
+session.add(category3)
+session.commit()
+
+category4 = Category(name="Skate Wheels")
+session.add(category4)
+session.commit()
+
+category5 = Category(name="Skate Trucks")
+session.add(category5)
+session.commit()
+
+category6 = Category(name="Bearings")
+session.add(category6)
+session.commit()
+
+category7 = Category(name="Helmets")
+session.add(category7)
+session.commit()
+
+category8 = Category(name="Griptape")
+session.add(category8)
+session.commit()
+
+category9 = Category(name="Stickers")
+session.add(category9)
+session.commit()
+
+# category10 = Category(name="T-Shirts")
+# session.add(category10)
+# session.commit()
+
+# category11 = Category(name="Shoes")
+# session.add(category11)
+# session.commit()
