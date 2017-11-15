cb80a44f62863a95d8d9b541d5537ad8458ae857
timwiesner <timothyjwiesner@gmail.com>
2017-11-15 08:39:25 -0600

    style: Modify for PEP8 compliance

--- db_setup.py
+++ db_setup.py
@@ -19,8 +19,8 @@ class Users(Base):
 class Category(Base):
     __tablename__ = 'category'
 
-    id = Column(Integer, primary_key = True)
-    name = Column(String(250), nullable = False)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
 
     @property
     def serialize(self):
@@ -34,8 +34,8 @@ class Category(Base):
 class Item(Base):
     __tablename__ = 'item'
 
-    name = Column(String(80), nullable = False)
-    id = Column(Integer, primary_key = True)
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
     description = Column(String(250))
     category_id = Column(Integer, ForeignKey('category.id'))
     category = relationship(Category)
