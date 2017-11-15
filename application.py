67ba2ae29b85077f68bfa37cd0e79679dfe180f2
timwiesner <timothyjwiesner@gmail.com>
2017-11-15 08:14:29 -0600

    fix: OAuth logout bug

--- application.py
+++ application.py
@@ -123,6 +123,28 @@ def gconnect():
     return output
 
 
+def createUser(login_session):
+    newUser = Users(name=login_session['username'],
+                    email=login_session['email'],
+                    picture=login_session['picture'])
+    session.add(newUser)
+    session.commit()
+    user = session.query(Users).filter_by(email=login_session['email']).one()
+    return user.id
+
+
+def getUserInfo(user_id):
+    user = session.query(Users).filter_by(id=user_id).one()
+    return user
+
+
+def getUserID(email):
+    try:
+        user = session.query(Users).filter_by(email=email).one()
+        return user.id
+    except:
+        return None
+
 
 # DISCONNECT - Revoke a current user's token and reset their login_session
 
