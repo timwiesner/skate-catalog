1d4113125f73b1ffb01d867b2adc58d3db71dc1d
timwiesner <timothyjwiesner@gmail.com>
2017-11-15 08:08:04 -0600

    feat: Add User base to db

--- db_setup.py
+++ db_setup.py
@@ -7,6 +7,15 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
+
+class Users(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
+
+
 class Category(Base):
     __tablename__ = 'category'
 
